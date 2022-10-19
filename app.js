81166bfc630b5d7d0d5da980d1dfe21069461206
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-10-19 09:54:16 +0700

    rename challange.js to app.js, and create logic when folder data/ and file contacts.json not created

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
+// Cek Folder "data/" Jika Tidak Ada Buat Folder-nya
+const dirPath = './data';
+if (!fs.existsSync(dirPath)) {
+    // > Buat folder baru dengan nama sesuai dengan variable dirPath
+    fs.mkdirSync(dirPath);
+}
+
+// Cek File "contacts.json" Jika Tidak Ada Buat File-nya
+const fileName = './data/contacts.json';
+if (!fs.existsSync(fileName)) {
+    // > Buat file baru dengan nama sesuai dengan variable fileName
+    // > Didalam method ini kita isikan:
+    // => nama file, isi file, format teks
+    fs.writeFileSync(fileName, "[]", "utf-8");
+}
+
 rl.question('Masukan Nama: ', (nama) => {
     rl.question('Masukan Nomor Ponsel: ', (ponsel) => {
         const objContact = {
