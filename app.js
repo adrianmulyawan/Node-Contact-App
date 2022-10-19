4160446f156c24e0bbb62ceeeff18036580ca1cb
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-10-19 10:36:46 +0700

    edit file app.js

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const {
 
 // Method Utama
 const main = async () => {
+    // > Membuat Pertanyaan
     const nama = await createQuestion("Masukan Nama Anda: ");
     const email = await createQuestion("Masukan Email Anda: ");
     const ponsel = await createQuestion("Masukan No Handphone Anda: ");
 
+    // > Method Simpan Contact
+    // => Nilai didapat dari Pertanyaan
     saveContact(nama, email, ponsel);
 };
 
